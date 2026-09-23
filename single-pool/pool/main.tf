a49870c6c48fc76ba46f1fadc7feb2d10a4b6960
anatolii2makaroff <anatolii.makaroff@gmail.com>
2026-09-22 20:16:47 -0400

    update ami

--- single-pool/pool/main.tf
+++ single-pool/pool/main.tf
@@ -35,7 +35,7 @@ module "pool" {
 
   description         = "Single pool"
   labels              = "name=pool-1,env=dev"
-  nodes_ami           = "ami-0352ea41018965493" # prebaked node AMI (see docs/node-amis)
+  nodes_ami           = "ami-0b14ba08cea3367cc" # prebaked node AMI (see docs/node-amis)
   nodes_instance_type = "c6gn.2xlarge"
   nodes_count         = 3
 
